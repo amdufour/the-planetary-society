29d9720d83eed465fd8885d7fea7df131c18f044
Anne-Marie Dufour <hello@annemariedufour.com>
2021-10-27 14:55:35 +0200

    Finalize data formatting

---
+++ js/dataPrep.js
@@ -0,0 +1,82 @@
+// Load data
+let promises = [d3.csv('../data/missions.csv'), d3.csv('../data/missions_cost_timeline.csv'), d3.csv('../data/missions_running_cost_timeline.csv'), d3.csv('../data/missions_cost_development_vs_operations.csv')];
+Promise.all(promises).then(data => {
+  prepData(data[0], data[1], data[2], data[3]);
+});
+
+const prepData = (missionsList, costTimelines, runningCostTimelines, devOpCost) => {
+  const missions = [
+    { destination: 'the_moon', missions: [] },
+    { destination: 'mars', missions: [] },
+    { destination: 'outer_planets', missions: [] },
+    { destination: 'venus', missions: [] },
+    { destination: 'small_bodies', missions: [] },
+    { destination: 'mercury', missions: [] },
+    { destination: 'other', missions: [] }
+  ];
+
+  missionsList.forEach(mission => {
+    mission['costTimelines'] = [];
+    mission['runningCostTimeline'] = [];
+    mission['devCost'] = devOpCost[0][mission.mission_id] ? devOpCost[0][mission.mission_id] : null,
+    mission['opCost'] = devOpCost[1][mission.mission_id] ? devOpCost[1][mission.mission_id] : null,
+    mission.fiscal_years_of_development = +mission.fiscal_years_of_development;
+    mission.launch_vehicle_cost_2020 = +mission.launch_vehicle_cost_2020;
+    mission.launch_year = +mission.launch_year;
+    
+    missions.find(m => m.destination === mission.destination).missions.push(mission);
+  });
+  console.log('missions', missions);
+
+  const updateMissionTimeline = (missionId, timeline, arrayToUpdate) => {
+    missions.forEach(destination => {
+      const currentMission = destination.missions.find(m => m.mission_id === missionId);
+      if (currentMission) {
+        currentMission[arrayToUpdate].push(timeline);
+      }
+    })
+  };
+
+  costTimelines.columns.forEach(col => {
+    const missionId = col;
+    let timeline = [];
+    if (missionId !== 'fiscal_year') {
+      const missionTimeline = costTimelines.filter(ct => ct[missionId] !== '0');
+      let currentYear = +missionTimeline[0].fiscal_year - 1;
+      
+      missionTimeline.forEach(y => {
+        if (currentYear !== +y.fiscal_year - 1) {
+          updateMissionTimeline(missionId, timeline, 'costTimelines');
+          timeline = [];
+        }
+
+        const cost = { year: +y.fiscal_year, cost: +y[missionId] };
+        timeline.push(cost);
+        currentYear = +y.fiscal_year;
+      });
+      updateMissionTimeline(missionId, timeline, 'costTimelines');
+    }
+  });
+
+  runningCostTimelines.columns.forEach(col => {
+    const missionId = col;
+    let timeline = [];
+    if (missionId !== 'fiscal_year') {
+      const missionTimeline = runningCostTimelines.filter(ct => ct[missionId] !== '0');
+      let currentYear = +missionTimeline[0].fiscal_year - 1;
+      
+      missionTimeline.forEach(y => {
+        if (currentYear !== +y.fiscal_year - 1) {
+          updateMissionTimeline(missionId, timeline, 'runningCostTimeline');
+          timeline = [];
+        }
+
+        const cost = { year: +y.fiscal_year, cost: +y[missionId] };
+        timeline.push(cost);
+        currentYear = +y.fiscal_year;
+      });
+      updateMissionTimeline(missionId, timeline, 'runningCostTimeline');
+    }
+  });
+
+};
--- js/main.js
+++ js/main.js
@@ -2,108 +2,49 @@ const margin = {top: 100, right: 30, bottom: 0, left: 100};
 const width = 1400;
 const height = 1000;
 const missionHeight = 200;
+const firstYear = 1960;
+const lastYear = 2022;
 
-// Load data
-let promises = [d3.csv('../data/missions.csv'), d3.csv('../data/missions_cost_timeline.csv'), d3.csv('../data/missions_running_cost_timeline.csv'), d3.csv('../data/missions_cost_development_vs_operations.csv')];
-Promise.all(promises).then(data => {
-  prepData(data[0], data[1], data[2], data[3]);
-});
+console.log('missions', missions);
 
-const prepData = (missionsList, costTimelines, runningCostTimelines, devOpCost) => {
-  console.log('missionsList', missionsList);
-  const missions = [
-    { destination: 'the_moon', missions: [] },
-    { destination: 'mars', missions: [] },
-    { destination: 'outer_planets', missions: [] },
-    { destination: 'venus', missions: [] },
-    { destination: 'small_bodies', missions: [] },
-    { destination: 'mercury', missions: [] },
-    { destination: 'other', missions: [] }
-  ];
+// Scales
+const yearsScale = d3.scaleLinear()
+  .domain([firstYear, lastYear])
+  .range([margin.left, width - margin.right - margin.left]);
+// const costScale = d3.scaleLinear()
+//   .domain([0, 10000]) // To refine based on data
+//   .range([missionHeight, 0]);
 
-  missionsList.forEach(mission => {
-    mission['costTimelines'] = [];
-    mission['runningCostTimeline'] = [];
-    mission['devCost'] = 0,
-    mission['opCost'] = 0,
-    mission.fiscal_years_of_development = +mission.fiscal_years_of_development;
-    mission.launch_vehicle_cost_2020 = +mission.launch_vehicle_cost_2020;
-    mission.launch_year = +mission.launch_year;
-    
-    missions.find(m => m.destination === mission.destination).missions.push(mission);
-  });
-  console.log('missions', missions);
+// Append SVG parent
+const svg = d3.select('#viz')
+  .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('viewbox', `0 0 ${width} ${height}`);
+
+// Append years axis
+const xAxis = svg
+  .append('g')
+    .attr('class', 'x-axis-container')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+  .call(d3.axisTop(yearsScale));
 
 
-  // const missions = [];
-  // const filteredData = data.filter(d => d.mars_perseverance_cost_inflation_adj !== 0);
-  // console.log('filteredData', filteredData);
+// Missions
+// const missionGroups = svg
+//   .append('g')
+//     .attr('class', 'missions')
+//   .selectAll('.mission-container')
+//   .data(missions)
+//   .join('g')
+//     .attr('class', d => `mission-container mission-container-${d.missionId}`);
 
-  // const missionInfo = {
-  //   missionId: 'mars_perseverance',
-  //   missionLabel: 'Mars Perseverance',
-  //   programLine: 'Flagship',
-  //   launchVehicleType: 'Mars rover',
-  //   budgetTimelines: []
-  // };
-
-  // let currentYear = d3.min(filteredData, d => d.fiscal_year) - 1;
-  // let budgetTimeline = [];
-  // filteredData.forEach(d => {
-  //   if (currentYear + 1 !== d.fiscal_year) {
-  //     missionInfo.budgetTimelines.push(budgetTimeline);
-  //     budgetTimeline = [];
-  //   }
-  //   budgetTimeline.push(d);
-  //   currentYear = d.fiscal_year;
-  // });
-  // missionInfo.budgetTimelines.push(budgetTimeline);
-  // missions.push(missionInfo);
-  // console.log('missions', missions);
-
-  // createViz(data, missions);
-};
-
-const createViz = (data, missions) => {
-
-  // Scales
-  const yearsScale = d3.scaleLinear()
-    .domain([d3.min(data, d => d.fiscal_year), d3.max(data, d => d.fiscal_year)])
-    .range([margin.left, width - margin.right - margin.left]);
-  const costScale = d3.scaleLinear()
-    .domain([0, 10000]) // To refine based on data
-    .range([missionHeight, 0]);
-
-  // Append SVG parent
-  const svg = d3.select('#viz')
-    .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('viewbox', `0 0 ${width} ${height}`);
-
-  // Append years axis
-  const xAxis = svg
-    .append('g')
-      .attr('class', 'x-axis-container')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`)
-    .call(d3.axisTop(yearsScale));
-
-
-  // Missions
-  const missionGroups = svg
-    .append('g')
-      .attr('class', 'missions')
-    .selectAll('.mission-container')
-    .data(missions)
-    .join('g')
-      .attr('class', d => `mission-container mission-container-${d.missionId}`);
-  
-  // Create one group for each timeline of the mission
-  const missionTimeline = missionGroups
-    .selectAll('.mission-timeline')
-    .data(d => d.costTimelines)
-    .join('g')
-      .attr('class', 'mission-timeline');
+// // Create one group for each timeline of the mission
+// const missionTimeline = missionGroups
+//   .selectAll('.mission-timeline')
+//   .data(d => d.costTimelines)
+//   .join('g')
+//     .attr('class', 'mission-timeline');
 
   // Append Mission name
 
@@ -118,5 +59,3 @@ const createViz = (data, missions) => {
   //     .attr('stroke', 'black');
 
   // Append cost curve
-
-};
